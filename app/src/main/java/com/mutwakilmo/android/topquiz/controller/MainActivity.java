d90a9634d8ada99efdd19c2b7a90477e088b8be0
kingkashta <mutwakil.m.osman@gmail.com>
2019-03-26 18:32:55 +0100

    Part #2 - Add Functionality

--- app/src/main/java/com/mutwakilmo/android/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/mutwakilmo/android/topquiz/controller/MainActivity.java
@@ -1,5 +1,6 @@
-package com.mutwakilmo.android.topquiz;
+package com.mutwakilmo.android.topquiz.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,11 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.mutwakilmo.android.topquiz.R;
+import com.mutwakilmo.android.topquiz.model.User;
+
 public class MainActivity extends AppCompatActivity {
 
    private TextView mGreetingText;
    private EditText mNameInput;
    private Button mPlayButton;
+   private User mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +28,7 @@ public class MainActivity extends AppCompatActivity {
        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
-
-
+       mUser = new User();
        // Disable and enable buttons based on user input: setEnabled()
        mPlayButton.setEnabled(false);
 
@@ -55,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
        mPlayButton.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
+               String firstname = mNameInput.getText().toString();
+               mUser.setFirstName(firstname);
                // the user just clicked
+               Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+               startActivity(gameActivityIntent);
 
            }
        });
@@ -64,5 +72,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    
+
 }
